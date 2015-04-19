481f5c6fe490f4105a6ca1458a101eb03e1d9713
dg4892 <dg4892@gmail.com>
2015-04-19 19:17:32 -0400

    3rd Commit

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+<?php
+	echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>';
+		echo "\n";
+	echo '<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.css">';
+		echo "\n";
+	echo '<script src="https://ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>';
+		echo "\n";
+	echo '<link rel="stylesheet" type="text/css" href="../css/styles.css">';
+		echo "\n";
+	echo '<script type="text/javascript" src="../scripts/scripts.js"></script>';
+?>
